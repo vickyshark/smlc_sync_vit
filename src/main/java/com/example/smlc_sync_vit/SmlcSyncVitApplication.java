8217945a51c3b40fe37fb87f2dc6cfdf9af22ced
Duc Le <vietduc.bk69@gmail.com>
2025-04-03 15:06:09 +0700

    add logs

--- src/main/java/com/example/smlc_sync_vit/SmlcSyncVitApplication.java
+++ src/main/java/com/example/smlc_sync_vit/SmlcSyncVitApplication.java
@@ -3,10 +3,15 @@ package com.example.smlc_sync_vit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class SmlcSyncVitApplication {
 
     public static void main(String[] args) {
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+        System.setProperty("logging.file.name", "logs/sync-" + timestamp + ".log");
         SpringApplication.run(SmlcSyncVitApplication.class, args);
     }
 
